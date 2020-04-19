4a1c4c6249e5e6854ff19817b921bef1253874b7
marimeli <marimel.casas@gmail.com>
2020-04-18 21:29:58 -0500

    header square

---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: Color(0xffff4081),
+    );
+  }
+}
